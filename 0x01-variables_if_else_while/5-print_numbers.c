a2bf69f75ef636541707b8da164f45f391df03ec
Excel97 <amarachiexcel4@gmail.com>
2022-04-09 13:23:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints decimal digits,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
+
